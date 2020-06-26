153b80d3c10ade41261f0172a4c5df51629ce033
rhico-sunAsterisk <rhico.abueme@sun-asterisk.com>
2020-06-26 18:40:37 +0800

    Made a login page. Modified account activation

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_200212) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
-    t.string "remember_digest"
-    t.boolean "admin", default: false
-    t.string "activation_digest"
-    t.boolean "activated", default: false
-    t.datetime "activated_at"
-    t.string "reset_digest"
-    t.datetime "reset_sent_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
+ActiveRecord::Schema.define(version: 2020_06_25_182150) do
 
 end
